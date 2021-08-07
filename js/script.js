32d41c36b0cf93e7ca9c0d2195f19e2a95c22c56
YogiSly <yogisly44@gmail.com>
2021-08-07 22:53:16 +0300

    create version html

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('#test-policy').onclick = function(){
+  document.getElementById('modal').style.display = 'block';
+}
+document.querySelector('#modal__close').onclick = function(){
+  document.getElementById('modal').style.display = 'none';
+}
