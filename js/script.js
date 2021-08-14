e1c9d25140029bca5d418d3f1a17fcb098ad0866
YogiSly <yogisly44@gmail.com>
2021-08-15 02:35:48 +0300

    append table - display table

--- js/script.js
+++ js/script.js
@@ -4,3 +4,27 @@ document.querySelector('#test-policy').onclick = function(){
 document.querySelector('#modal__close').onclick = function(){
   document.getElementById('modal').style.display = 'none';
 }
+
+
+var menu = new Vue({
+		el: '#sidebar',
+		data: {
+			list:[
+				{title: 'Policy management',
+					link: 'pol-list.html'},
+				{title: 'Account management',
+					link: 'acc-list.html'},
+				{title: 'User management',
+					link: 'user-list.html'},
+			],
+			
+			isActive: true,
+
+		},
+	method: {
+		classActive: function(){
+			this.classActive = true;
+		}
+	}
+
+})
